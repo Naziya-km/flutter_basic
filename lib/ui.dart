1d63843d35aefd55f15857dc46c2c3c6f06d83dd
Naziya-km <naziyakm2000@gmail.com>
2024-07-01 13:42:40 +0530

    giving class

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,92 +1,99 @@
+import 'package:demo_app/items.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ui extends StatelessWidget {
   ui({super.key});
 
-  List names = ["irul cair", "malik cahir", "good chair"];
+  var prdouct = Item();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                    offset: Offset(2, 2), color: Colors.grey, blurRadius: 20)
-              ],
-              color: Color.fromARGB(255, 255, 237, 237),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            margin: EdgeInsets.all(10),
-            height: 300,
-            child: Row(
-              // mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 50,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1612372606404-0ab33e7187ee?q=80&w=978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")),
-                    // color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      names[index],
-                      style:
-                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+        body: Column(children: [
+      Text("products"),
+      Expanded(
+          child: ListView.builder(
+              itemCount: prdouct.items.length,
+              itemBuilder: (context, index) {
+                return Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(2, 2),
+                            color: Colors.grey,
+                            blurRadius: 20)
+                      ],
+                      color: Color.fromARGB(255, 255, 237, 237),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text("ITS A GOOD CHAIR"),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text("COMFORTABLE"),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
+                    margin: EdgeInsets.all(10),
+                    height: 300,
+                    child: Row(
+                      // mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text("RS 1200/-"),
                         SizedBox(
                           width: 50,
                         ),
-                        ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Color.fromARGB(255, 19, 18, 19))),
-                            onPressed: () {},
-                            child: Text(
-                              "BUY",
-                              style: TextStyle(color: Colors.red),
-                            )),
+                        Container(
+                          height: 200,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    prdouct.items[index]["image"])),
+                            // color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 100,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 250,
+                              child: Text(
+                                prdouct.items[index]["title"],
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text("ITS A GOOD CHAIR"),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text("COMFORTABLE"),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(children: [
+                              Text("Rs. ${prdouct.items[index]["price"]}"),
+                              SizedBox(
+                                width: 50,
+                              ),
+                              ElevatedButton(
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Color.fromARGB(255, 19, 18, 19))),
+                                  onPressed: () {},
+                                  child: Text(
+                                    "BUY",
+                                    style: TextStyle(color: Colors.red),
+                                  )),
+                            ]),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+                    ));
+              }))
+    ]));
   }
 }
